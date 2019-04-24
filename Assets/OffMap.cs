c9ebf1c3583005347897672b5f753f79479c013d
Nicholas Andrada <nandrada@csumb.edu>
2019-04-24 15:07:46 -0700

    Added reset boxes and bounce cones to floor after pipes 2 on Hard Level. Also added a bounce material

---
+++ Assets/OffMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OffMap : MonoBehaviour
+{
+	private void Start()
+	{
+		Debug.Log("started the offMap Script");
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Debug.Log("Player has entered area of no return");
+	}
+}
