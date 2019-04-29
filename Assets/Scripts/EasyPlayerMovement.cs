edea7cfe01ee9e0b58548d418f74505da8f662a1
Lucas Montanari <luke@Lucass-MacBook-Air.local>
2019-04-29 12:07:04 -0700

    added first level layout

--- Assets/Scripts/EasyPlayerMovement.cs
+++ Assets/Scripts/EasyPlayerMovement.cs
@@ -7,6 +7,7 @@ public class EasyPlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public int jumpHeight;
     private Rigidbody rb;
 
 	// Start is called before the first frame update
@@ -15,7 +16,7 @@ public class EasyPlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 	 }
 
-	 private void FixedUpdate()
+    private void FixedUpdate()
 	 {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical"); 
@@ -23,9 +24,59 @@ public class EasyPlayerMovement : MonoBehaviour
 
         rb.AddForce(movement * speed);
 
-        if(Input.GetKeyDown(KeyCode.Space) && rb.transform.position.y <= 1.5)
+        // setting up a ray and a hit for the raycast
+        RaycastHit hit;
+        Ray jump = new Ray(transform.position, Vector3.down);
+        // if space is pressed and the ray has hit at the jumpHeight, jump.
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(jump, out hit, jumpHeight))
         {
             rb.AddForce(0.0f, jumpForce, 0.0f);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("End"))
+        {
+            StartCoroutine(endGame());
+            // go to start for now
+            //transform.position = new Vector3(1.5f, 1.1f, 0.5f);
+            // write code to go to the next scene
+
+        }
+        else if(other.CompareTag("Respawn"))
+        {
+            transform.position = new Vector3(1.5f, 1.1f, 0.5f);
+        }else if(other.CompareTag("Easter"))
+        {
+            // go to start for now 
+            transform.position = new Vector3(1.5f, 1.1f, 0.5f);
+            // write code to pause the scene, 
+            // display that the easter egg has been found, 
+            // display the creator of the level for this level
+            // think of more options to do for an easter egg ex. colors, ball attributes, etc...
+        }
+    }
+
+    public IEnumerator endGame()
+    {
+        yield return new WaitForSeconds(3);
+
+        transform.position = new Vector3(1.5f, 1.1f, 0.5f);
+
+    }
+
+    public IEnumerator easterEgg()
+    {
+        // display that the easter egg has been found,
+        // display the creator of the level for this level
+
+        // wait for 5 seconds
+        yield return new WaitForSeconds(5);
+
+        // think of more options to do for an easter egg ex. colors, ball attributes, etc...
+        
+        // go to start  
+        transform.position = new Vector3(1.5f, 1.1f, 0.5f);
     }
 }
--- Assets/Scripts/cameraController2.cs
+++ Assets/Scripts/cameraController2.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraController : MonoBehaviour
+public class cameraController2 : MonoBehaviour
 {
+    public GameObject player;
+    public Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // calculating the distance between the player and the camera
+        offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        // updating the camera to that position every frame
+        transform.position = player.transform.position + offset;
     }
 }
