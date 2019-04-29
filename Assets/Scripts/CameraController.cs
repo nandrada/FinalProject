8613eaaad6d2974a8e9e219fe4f87313dac7e4ca
Nicholas Andrada <nandrada@csumb.edu>
2019-04-29 14:47:58 -0700

    Finished Hard Level 1 and started on Hard Level 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,13 @@
-﻿	using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
 	public GameObject player;
 	public Vector3 offset;
+	public float RotateSpeed = 30f;
 
 	// Start is called before the first frame update
 	void Start()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float speed;
 	private Rigidbody rb;
+	public float RotateSpeed = 30.0f;
 
 	// Start is called before the first frame update
 	void Start()
