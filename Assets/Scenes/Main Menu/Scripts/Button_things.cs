9ee005dcb8ed05a8c03944f0a8ab42bed0b4be34
unknown <acasillas@csumb.edu>
2019-04-30 12:28:20 -0700

    Main menu

---
+++ Assets/Scenes/Main Menu/Scripts/Button_things.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_things : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scenes/Main Menu/Scripts/Hard_Maps.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Hard_Maps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Back()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
